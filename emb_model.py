a307d2356937f9a02d6c96afe5636f88b8a21ade
Gazizov Marsel <MarseL123@users.noreply.huggingface.co>
2024-07-16 15:32:38 +0000

    Switch to model 1

--- emb_model.py
+++ emb_model.py
@@ -9,11 +9,11 @@ from transformers import pipeline
 
 #model 1:
 
-# model_sent_embedding = SentenceTransformer("all-MiniLM-L6-v2")
+model_sent_embedding = SentenceTransformer("all-MiniLM-L6-v2")
 
-# def get_embeddings_from_sentences(sentences):
-#   embeddings = model_sent_embedding.encode(sentences, convert_to_tensor=True)
-#   return embeddings
+def get_embeddings_from_sentences(sentences):
+  embeddings = model_sent_embedding.encode(sentences, convert_to_tensor=True)
+  return embeddings
 
 #model 2:
 
@@ -26,18 +26,18 @@ from transformers import pipeline
 #model 3:
 
 # model_sent_embedding = pipeline("feature-extraction", model="google/canine-c")
-from transformers import CanineTokenizer, CanineModel
-model = CanineModel.from_pretrained('google/canine-c')
-tokenizer = CanineTokenizer.from_pretrained('google/canine-c')
+#from transformers import CanineTokenizer, CanineModel
+#model = CanineModel.from_pretrained('google/canine-c')
+#tokenizer = CanineTokenizer.from_pretrained('google/canine-c')
 
-def get_embeddings_from_sentences(sentences):
-  encoding = tokenizer(sentences, padding="longest", truncation=True, return_tensors="pt")
+#def get_embeddings_from_sentences(sentences):
+#  encoding = tokenizer(sentences, padding="longest", truncation=True, return_tensors="pt")
 
-  outputs = model(**encoding) # forward pass
+#  outputs = model(**encoding) # forward pass
 
-  pooled_output = outputs.pooler_output
-  sequence_output = outputs.last_hidden_state
-  return pooled_output
+#  pooled_output = outputs.pooler_output
+#  sequence_output = outputs.last_hidden_state
+#  return pooled_output
 
 
 ########################################
