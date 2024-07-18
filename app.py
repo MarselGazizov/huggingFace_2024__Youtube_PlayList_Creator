6764806d41f1ab4f038a4193a9f80bcc7760d61a
Marsel <autoacceleration42@gmail.com>
2024-07-19 00:31:32 +0300

    changed options in graph

--- app.py
+++ app.py
@@ -219,7 +219,17 @@ def get_pipeline_prediction(channel_id, rate: float, amount_of_videos):
     # save_json(df_clusters, "clusters.json")
 
     pipeline_output['graph_pyvis'].show_buttons(filter_=['physics'])
-    pipeline_output['graph_pyvis'].barnes_hut(gravity=-28050)
+    options = """
+    const options = {
+      "physics": {
+        "barnesHut": {
+          "gravitationalConstant": -28050
+        },
+        "minVelocity": 0.75
+      }
+    }
+    """
+    pipeline_output['graph_pyvis'].set_options(options)
     pipeline_output['graph_pyvis'].save_graph("networkx-pyvis.html")
     # f = open("networkx-pyvis.html")
 
