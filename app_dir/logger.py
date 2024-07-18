88227a2d44e1175b0d8f97a7e778e1564d73adbd
Marsel <autoacceleration42@gmail.com>
2024-07-18 19:17:16 +0300

    changed files; logging stuff

--- app_dir/logger.py
+++ app_dir/logger.py
@@ -4,6 +4,7 @@ import logging
 # formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
 def get_logger(name, level):
+    logging.basicConfig()
     log = logging.getLogger(name)
     log.setLevel(level)
     return log
