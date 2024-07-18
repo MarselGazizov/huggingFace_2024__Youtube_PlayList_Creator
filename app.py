05016ebeeca71fb69f45194daa39c7ba679a587e
Marsel <autoacceleration42@gmail.com>
2024-07-18 11:53:57 +0300

    format change

--- app.py
+++ app.py
@@ -63,18 +63,7 @@ For more information on `huggingface_hub` Inference API support, please check th
 #     demo.launch()
 
 
-
-
-
-
-
-
-
-
-
-
 #######################
-
 
 
 ### libraries
@@ -91,15 +80,11 @@ For more information on `huggingface_hub` Inference API support, please check th
 # !pip install -q -U transformers gradio
 from transformers import pipeline
 from transformers.utils import logging
-logging.set_verbosity_error()
 
+logging.set_verbosity_error()
 
 # !pip install --upgrade google-api-python-client
 # from google.colab import userdata
-
-
-
-
 
 
 """### vars"""
@@ -110,65 +95,55 @@ import os
 HF_TOKEN_F = os.getenv('HF_TOKEN_F')
 API_KEY = os.getenv('api_key')
 
-
-
-
-
-
-
-
 """### data"""
 from apiclient.discovery import build
 
 youtube = build('youtube', 'v3', developerKey=API_KEY)
 
+
 class Data_gen:
+    videos = []
 
-  videos = []
+    _youtube = None
 
-  _youtube = None
+    def __init__(self, youtube):
+        self._youtube = youtube
 
-  def __init__(self, youtube):
-    self._youtube = youtube
+    def get_all_videos_from_youtube_chanel_that_is_on_native_lang(self, channel_id):
 
-  def get_all_videos_from_youtube_chanel_that_is_on_native_lang(self, channel_id):
+        res = self._youtube.channels().list(id=channel_id,
+                                            part='contentDetails').execute()
 
-    res = self._youtube.channels().list(id=channel_id,
-                                  part='contentDetails').execute()
+        playlist_id = res['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
-    playlist_id = res['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+        next_page_token = None
 
-    next_page_token = None
+        while True:
+            res = self._youtube.playlistItems().list(playlistId=playlist_id,
+                                                     part='snippet',
+                                                     maxResults=50,
+                                                     pageToken=next_page_token).execute()
+            self.videos += res['items']
+            next_page_token = res.get('nextPageToken')
 
-    while True:
-        res = self._youtube.playlistItems().list(playlistId=playlist_id,
-                                           part='snippet',
-                                           maxResults=50,
-                                           pageToken=next_page_token).execute()
-        self.videos += res['items']
-        next_page_token = res.get('nextPageToken')
+            if next_page_token is None:
+                break
 
-        if next_page_token is None:
-            break
+        return self.videos
 
-    return self.videos
+    def get_titles_of_videos_data(self, amount=500, get_all=False):
+        if (get_all):
+            r = self.videos.copy()
+        else:
+            r = self.videos.copy()[:amount]
+        for i in range(len(r)):
+            r[i] = self.videos[i]['snippet']['title']
+        return r
 
-  def get_titles_of_videos_data(self, amount=500, get_all=False):
-    if(get_all):
-      r = self.videos.copy()
-    else:
-      r = self.videos.copy()[:amount]
-    for i in range(len(r)):
-      r[i] = self.videos[i]['snippet']['title']
-    return r
 
 data_gen = Data_gen(
-    youtube = youtube
-    )
-
-
-
-
+    youtube=youtube
+)
 
 """
 ### models
@@ -193,10 +168,7 @@ data_gen = Data_gen(
 # translator_to_eng = Translator_to_eng(pipe_tr)
 
 
-
 """#### translating"""
-
-
 
 # def get_all_videos_from_youtube_chanel_and_turn_to_eng_lang(channel_id):
 #   return list(map(translator_to_eng.translate, get_all_videos_from_youtube_chanel_that_is_on_native_lang(channel_id)))
@@ -215,7 +187,6 @@ data_gen = Data_gen(
 # videos_both_native_and_eng.append(all_videos_from_yout_chanel_that_is_on_eng_lang)
 
 
-
 # title_of_videos_both_native_and_eng = dict()
 # title_of_videos_both_native_and_eng['native'] = get_titles_of_videos_data(
 #     videos_both_native_and_eng['native']
@@ -225,7 +196,6 @@ data_gen = Data_gen(
 # )
 
 
-
 """#### model( embedding )"""
 
 # pip install thread6
@@ -233,10 +203,6 @@ data_gen = Data_gen(
 
 
 from emb_model import get_most_similar_sentences
-
-
-
-
 
 """#### summarization"""
 
@@ -262,7 +228,6 @@ chatbot = pipeline("text2text-generation", model="google/flan-t5-large")
 #   return res
 
 
-
 """### graphs"""
 
 # pip install pyvis
@@ -273,26 +238,26 @@ import networkx as nx
 from pyvis.network import Network
 from IPython.display import display, HTML
 
+
 def get_graphs_and_cmp_sv(sentences_to_comp, rate):
-  graph = nx.Graph()
-  graph.add_nodes_from(sentences_to_comp)
-  edjes = get_most_similar_sentences(sentences_to_comp, rate)
-  graph.add_edges_from(edjes)
+    graph = nx.Graph()
+    graph.add_nodes_from(sentences_to_comp)
+    edjes = get_most_similar_sentences(sentences_to_comp, rate)
+    graph.add_edges_from(edjes)
 
-  dict_res = {
-      "comp_sv": list(nx.connected_components(graph)),
-      "amount_of_comp_sv": nx.number_connected_components(graph),
-      "graph_nx": graph
-  }
+    dict_res = {
+        "comp_sv": list(nx.connected_components(graph)),
+        "amount_of_comp_sv": nx.number_connected_components(graph),
+        "graph_nx": graph
+    }
 
-  net = Network(notebook=True, cdn_resources='remote')
-  net.add_nodes(sentences_to_comp)
-  net.add_edges(edjes)
+    net = Network(notebook=True, cdn_resources='remote')
+    net.add_nodes(sentences_to_comp)
+    net.add_edges(edjes)
 
-  dict_res['graph_pyvis'] = net
+    dict_res['graph_pyvis'] = net
 
-  return dict_res
-
+    return dict_res
 
 
 # pip install community
@@ -300,47 +265,41 @@ def get_graphs_and_cmp_sv(sentences_to_comp, rate):
 
 import community.community_louvain as lo
 
+
 def get_clusters(graph_nx):
-  partition = lo.best_partition(graph_nx)
+    partition = lo.best_partition(graph_nx)
 
-  clusters = []
+    clusters = []
 
-
-  for cluster_id in set(partition.values()):
-      nodes = [nodes for nodes in partition.keys() if partition[nodes] == cluster_id]
-      if(len(nodes)>2):
-        clusters.append(nodes)
-  return clusters
-
+    for cluster_id in set(partition.values()):
+        nodes = [nodes for nodes in partition.keys() if partition[nodes] == cluster_id]
+        if (len(nodes) > 2):
+            clusters.append(nodes)
+    return clusters
 
 
 """### wrappers for models"""
 
+
 def imp_func(youtube_chanel_id, rate=0.75, amount_of_max_videos=500, get_all=False):
-  videos = data_gen.get_all_videos_from_youtube_chanel_that_is_on_native_lang(channel_id=youtube_chanel_id)
-  videos_to_comp = data_gen.get_titles_of_videos_data(amount=amount_of_max_videos, get_all=get_all)
+    videos = data_gen.get_all_videos_from_youtube_chanel_that_is_on_native_lang(channel_id=youtube_chanel_id)
+    videos_to_comp = data_gen.get_titles_of_videos_data(amount=amount_of_max_videos, get_all=get_all)
 
-  gr_res = get_graphs_and_cmp_sv(videos_to_comp, rate)
-  clusters = get_clusters(gr_res['graph_nx'])
-  print(clusters)
+    gr_res = get_graphs_and_cmp_sv(videos_to_comp, rate)
+    clusters = get_clusters(gr_res['graph_nx'])
+    print(clusters)
 
-  # import gc
-  # del model_sent_embedding
-  # gc.collect()
+    # import gc
+    # del model_sent_embedding
+    # gc.collect()
 
-  dict_res = {}
-  dict_res['graph_pyvis'] = gr_res['graph_pyvis']
-  dict_res['clusters'] = clusters
+    dict_res = {}
+    dict_res['graph_pyvis'] = gr_res['graph_pyvis']
+    dict_res['clusters'] = clusters
 
-  print(f"___DEBUG___/ imp_func( {youtube_chanel_id},{rate},{amount_of_max_videos},{get_all} )/ dict_res:{dict_res}")
+    print(f"___DEBUG___/ imp_func( {youtube_chanel_id},{rate},{amount_of_max_videos},{get_all} )/ dict_res:{dict_res}")
 
-  return dict_res
-
-
-
-
-
-
+    return dict_res
 
 
 """# gradio"""
@@ -349,6 +308,7 @@ def imp_func(youtube_chanel_id, rate=0.75, amount_of_max_videos=500, get_all=Fal
 
 import os
 import gradio as gr
+
 
 # import matplotlib.cm as cm
 # import numpy as np
@@ -361,8 +321,8 @@ import gradio as gr
 #     return color_hex
 
 def get_pipeline_prediction(channel_id, rate: float, amount_of_videos):
-
-    pipeline_output = imp_func(youtube_chanel_id=channel_id, amount_of_max_videos=amount_of_videos, rate=rate, get_all=False)
+    pipeline_output = imp_func(youtube_chanel_id=channel_id, amount_of_max_videos=amount_of_videos, rate=rate,
+                               get_all=False)
 
     res = []
 
@@ -372,19 +332,16 @@ def get_pipeline_prediction(channel_id, rate: float, amount_of_videos):
     n = len(pipeline_output['clusters'])
 
     for i in range(n):
-      colors.append('#%06X' % randint(0, 0xFFFFFF))
+        colors.append('#%06X' % randint(0, 0xFFFFFF))
 
     # #b
     # for node in pipeline_output['graph_pyvis'].get_nodes():
     #   print(node)
     # #/b
 
-
     for i in pipeline_output['clusters']:
-      res.append(i)
-      res.append(["______"])
-
-
+        res.append(i)
+        res.append(["______"])
 
     pipeline_output['graph_pyvis'].show_buttons(filter_=['physics'])
     pipeline_output['graph_pyvis'].save_graph("networkx-pyvis.html")
@@ -400,7 +357,6 @@ def get_pipeline_prediction(channel_id, rate: float, amount_of_videos):
     #   attrs['color'] = node_degrees[node_id]
     # # /colorizing graph
 
-
     # gr_nx = gr_res['graph_nx']
     # import plotly.express as px
 
@@ -415,16 +371,15 @@ def get_pipeline_prediction(channel_id, rate: float, amount_of_videos):
     # f = open("networkx-pyvis.html")
     return (res, "networkx-pyvis.html")
 
-
-
     # HTML(filename="networkx-pyvis.html")
 
     # return res
 
+
 demo = gr.Interface(
-  fn=get_pipeline_prediction,
-  inputs = [gr.Text(label="channel id", type="text"), gr.Slider(0, 1, step=0.05, value=0.8), gr.Number()],
-  outputs=[gr.Text(type="text"), gr.File(file_types=[".html"])]
+    fn=get_pipeline_prediction,
+    inputs=[gr.Text(label="channel id", type="text"), gr.Slider(0, 1, step=0.05, value=0.8), gr.Number()],
+    outputs=[gr.Text(type="text"), gr.File(file_types=[".html"])]
 )
 
 # demo = gr.Blocks()
@@ -445,9 +400,9 @@ demo = gr.Interface(
 #                inputs=inp,
 #                outputs=out)
 
-#UCdxesVp6Fs7wLpnp1XKkvZg
-#UCuXYmUOJSbEH1x88WUV1aMg
-#UCuXYmUOJSbEH1x88WUV1aMg
+# UCdxesVp6Fs7wLpnp1XKkvZg
+# UCuXYmUOJSbEH1x88WUV1aMg
+# UCuXYmUOJSbEH1x88WUV1aMg
 
 # demo.launch(share=True,debug=True)
 if __name__ == "__main__":
--- emb_model.py
+++ emb_model.py
@@ -7,15 +7,17 @@ from transformers import pipeline
 
 ########################################
 
-#model 1:
+# model 1:
 
 model_sent_embedding = SentenceTransformer("all-MiniLM-L6-v2")
 
-def get_embeddings_from_sentences(sentences):
-  embeddings = model_sent_embedding.encode(sentences, convert_to_tensor=True)
-  return embeddings
 
-#model 2:
+def get_embeddings_from_sentences(sentences):
+    embeddings = model_sent_embedding.encode(sentences, convert_to_tensor=True)
+    return embeddings
+
+
+# model 2:
 
 # model_sent_embedding = SentenceTransformer("all-MiniLM-L6-v2")
 
@@ -23,14 +25,14 @@ def get_embeddings_from_sentences(sentences):
 #   embeddings = model_sent_embedding.encode(sentences, convert_to_tensor=True)
 #   return embeddings
 
-#model 3:
+# model 3:
 
 # model_sent_embedding = pipeline("feature-extraction", model="google/canine-c")
-#from transformers import CanineTokenizer, CanineModel
-#model = CanineModel.from_pretrained('google/canine-c')
-#tokenizer = CanineTokenizer.from_pretrained('google/canine-c')
+# from transformers import CanineTokenizer, CanineModel
+# model = CanineModel.from_pretrained('google/canine-c')
+# tokenizer = CanineTokenizer.from_pretrained('google/canine-c')
 
-#def get_embeddings_from_sentences(sentences):
+# def get_embeddings_from_sentences(sentences):
 #  encoding = tokenizer(sentences, padding="longest", truncation=True, return_tensors="pt")
 
 #  outputs = model(**encoding) # forward pass
@@ -44,87 +46,89 @@ def get_embeddings_from_sentences(sentences):
 
 
 def how_similar_sentences(sentences):
-  r = get_embeddings_from_sentences(sentences)
-  # return model.similarity(r,r)
-  return util.cos_sim(r,r)
+    r = get_embeddings_from_sentences(sentences)
+    # return model.similarity(r,r)
+    return util.cos_sim(r, r)
+
 
 def get_most_similar_sentences(sentences, rate_in_mtrx=0.7):
-  mtrx = how_similar_sentences(sentences)
-  arr_res = []
-  # todo debug sentences
+    mtrx = how_similar_sentences(sentences)
+    arr_res = []
+    # todo debug sentences
 
-  s = Semaphore(1)
+    s = Semaphore(1)
 
-  def fill_j(i):
+    def fill_j(i):
 
-    # #first version
-    # arr = []
-    # # rand = random.random()
-    # for j in range(i+1, len(sentences)):
-    #   # print(f"potok with r={rand}, max_eq={max_eq}")
-    #   if(i==j):
-    #     print("ERROR")
-    #   if(mtrx[i][j]>=rate_in_mtrx):
-    #     # arr.append([sentences[i],sentences[j]])
-    #     arr.append([sentences[i], sentences[j]])
-    # # arr.append((sentences[max_index[0]],sentences[max_index[1]]))
-    # # arr.sort()
-    # # s.acquire()
-    # # # arr_res.extend(arr)
-    # # arr_res.add(arr)
-    # # print(f"potok ext arr_res with: {arr}\n")
-    # # s.release()
+        # #first version
+        # arr = []
+        # # rand = random.random()
+        # for j in range(i+1, len(sentences)):
+        #   # print(f"potok with r={rand}, max_eq={max_eq}")
+        #   if(i==j):
+        #     print("ERROR")
+        #   if(mtrx[i][j]>=rate_in_mtrx):
+        #     # arr.append([sentences[i],sentences[j]])
+        #     arr.append([sentences[i], sentences[j]])
+        # # arr.append((sentences[max_index[0]],sentences[max_index[1]]))
+        # # arr.sort()
+        # # s.acquire()
+        # # # arr_res.extend(arr)
+        # # arr_res.add(arr)
+        # # print(f"potok ext arr_res with: {arr}\n")
+        # # s.release()
 
-    # # todo fix array indexes
-    # s.acquire()
-    # arr_res.extend(arr)
-    # print(f"potok ext arr_res with: {arr}\n")
-    # s.release()
+        # # todo fix array indexes
+        # s.acquire()
+        # arr_res.extend(arr)
+        # print(f"potok ext arr_res with: {arr}\n")
+        # s.release()
 
-    #second version
-    # arr =
-    max_eq = 0
-    max_index = [-1, -1]
-    # rand = random.random()
-    for j in range(i+1, len(sentences)):
-      # print(f"potok with r={rand}, max_eq={max_eq}")
-      if(i==j):
-        print("ERROR")
-      if(mtrx[i][j]>=rate_in_mtrx and mtrx[i][j]>max_eq):
-        # arr.append([sentences[i],sentences[j]])
-        max_eq = mtrx[i][j]
-        max_index = [i, j]
-    # arr.append((sentences[max_index[0]],sentences[max_index[1]]))
-    # arr.sort()
-    # s.acquire()
-    # # arr_res.extend(arr)
-    # arr_res.add(arr)
-    # print(f"potok ext arr_res with: {arr}\n")
-    # s.release()
-    if(max_index[0] != -1 and max_index[1] != -1):
-      if(max_index[0]==max_index[1]):
-        print("ERROR_2")
-      t = [sentences[max_index[0]],sentences[max_index[1]]]
-      if(sentences[max_index[0]]==sentences[max_index[1]]):
-        print(f"ERROR_3: {sentences[max_index[0]]==sentences[max_index[1]]}, i={max_index[0]}, j={max_index[1]}")
-      else:
-        s.acquire()
-        arr_res.append(t)
-        print(f"potok ext arr_res with: {t}\n")
-        s.release()
+        # second version
+        # arr =
+        max_eq = 0
+        max_index = [-1, -1]
+        # rand = random.random()
+        for j in range(i + 1, len(sentences)):
+            # print(f"potok with r={rand}, max_eq={max_eq}")
+            if (i == j):
+                print("ERROR")
+            if (mtrx[i][j] >= rate_in_mtrx and mtrx[i][j] > max_eq):
+                # arr.append([sentences[i],sentences[j]])
+                max_eq = mtrx[i][j]
+                max_index = [i, j]
+        # arr.append((sentences[max_index[0]],sentences[max_index[1]]))
+        # arr.sort()
+        # s.acquire()
+        # # arr_res.extend(arr)
+        # arr_res.add(arr)
+        # print(f"potok ext arr_res with: {arr}\n")
+        # s.release()
+        if (max_index[0] != -1 and max_index[1] != -1):
+            if (max_index[0] == max_index[1]):
+                print("ERROR_2")
+            t = [sentences[max_index[0]], sentences[max_index[1]]]
+            if (sentences[max_index[0]] == sentences[max_index[1]]):
+                print(
+                    f"ERROR_3: {sentences[max_index[0]] == sentences[max_index[1]]}, i={max_index[0]}, j={max_index[1]}")
+            else:
+                s.acquire()
+                arr_res.append(t)
+                print(f"potok ext arr_res with: {t}\n")
+                s.release()
 
-  threads = []
-  for i in range(len(sentences)):
-    t1 = Thread(target=fill_j, args=(i,), daemon=True)
-    threads.append(t1)
-    t1.start()
+    threads = []
+    for i in range(len(sentences)):
+        t1 = Thread(target=fill_j, args=(i,), daemon=True)
+        threads.append(t1)
+        t1.start()
 
-  for t in threads:
-    t.join()
+    for t in threads:
+        t.join()
 
-  print(f"\n{len(threads)}\n")
+    print(f"\n{len(threads)}\n")
 
-  for t in threads:
-    print(t.is_alive())
+    for t in threads:
+        print(t.is_alive())
 
-  return arr_res
+    return arr_res
